5a20470fd2eb95f9fb51112f8edb667c759ab899
yoshoku <yoshoku@outlook.com>
2025-08-07 16:05:21 +0900

    feat: implement seven-segment display component

--- ui/components/UISevenSegmentDisplay.vue
+++ ui/components/UISevenSegmentDisplay.vue
@@ -1,11 +1,58 @@
 <template>
     <div className="ui-seven-segment-display-wrapper">
-        This is a Seven Segment Display widget.
+        <div
+            class="segment horizontal segment-a"
+            :class="{ active: segments.a }"
+        />
+
+        <div class="middle-row">
+            <div
+                class="segment vertical segment-f"
+                :class="{ active: segments.f }"
+            />
+            <div
+                class="segment vertical segment-b"
+                :class="{ active: segments.b }"
+            />
+        </div>
+
+        <div
+            class="segment horizontal segment-g"
+            :class="{ active: segments.g }"
+        />
+
+        <div class="middle-row">
+            <div
+                class="segment vertical segment-e"
+                :class="{ active: segments.e }"
+            />
+            <div
+                class="segment vertical segment-c"
+                :class="{ active: segments.c }"
+            />
+        </div>
+
+        <div
+            class="segment horizontal segment-d"
+            :class="{ active: segments.d }"
+        />
+
+        <div
+            class="segment dot segment-dot"
+            :class="{ active: segments.dp }"
+        />
     </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+
+const KEYS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'dp'].reverse()
+const normalizeNum = (num) => Math.max(0, Math.min(255, Math.floor(num)))
+const convertSegments = (str) => Object.fromEntries(
+    parseInt(str, 16).toString(2).padStart(8, '0')
+        .split('').map((bit, id) => [KEYS[id], bit === '1'])
+)
 
 export default {
     name: 'UISevenSegmentDisplay',
@@ -18,21 +65,28 @@ export default {
     },
     computed: {
         ...mapState('data', ['messages']),
-        value: function () {
+        segments: function () {
+            let hexStr = '0'
             switch (this.props.valueType) {
             case 'msg': {
                 const msg = this.messages[this.id]
                 if (msg && Object.prototype.hasOwnProperty.call(msg, this.props.value)) {
-                    return msg[this.props.value]
+                    if (typeof msg[this.props.value] === 'number') {
+                        hexStr = normalizeNum(msg[this.props.value]).toString(16)
+                    } else {
+                        hexStr = msg[this.props.value] || '0'
+                    }
                 }
                 break
             }
-            case 'str':
-                return parseInt(this.props.value, 16).toString(2)
             case 'num':
-                return this.props.value.toString(2)
+                hexStr = normalizeNum(this.props.value || 0).toString(16)
+                break
+            case 'str':
+                hexStr = this.props.value || '0'
+                break
             }
-            return 1
+            return convertSegments(hexStr)
         }
     },
     created () {
@@ -88,9 +142,99 @@ export default {
 
 <style scoped>
 .ui-seven-segment-display-wrapper {
+    position: relative;
+    width: 120px;
+    height: 200px;
+    margin: 20px auto;
+    background-color: #000;
+    padding: 10px;
+    border-radius: 8px;
     display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    gap: 2px;
+    flex-direction: column;
+    justify-content: space-between;
+}
+
+.segment {
+    background-color: #666;
+}
+
+.segment.active {
+    background-color: #f00;
+}
+
+.horizontal {
+    height: 12px;
+    position: relative;
+    margin: 0 12px;
+    border-radius: 2px;
+}
+
+.horizontal::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: -6px;
+    right: -6px;
+    height: 100%;
+    background: inherit;
+    clip-path: polygon(6px 0%, calc(100% - 6px) 0%, 100% 50%, calc(100% - 6px) 100%, 6px 100%, 0% 50%);
+}
+
+.vertical {
+    width: 12px;
+    height: 70px;
+    position: relative;
+    border-radius: 2px;
+}
+
+.vertical::before {
+    content: '';
+    position: absolute;
+    top: -6px;
+    bottom: -6px;
+    left: 0;
+    width: 100%;
+    background: inherit;
+    clip-path: polygon(0% 6px, 50% 0%, 100% 6px, 100% calc(100% - 6px), 50% 100%, 0% calc(100% - 6px));
+}
+
+.segment-a {
+    align-self: stretch;
+}
+
+.segment-g {
+    align-self: stretch;
+}
+
+.segment-d {
+    align-self: stretch;
+}
+
+.middle-row {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.segment-f, .segment-e {
+    align-self: flex-start;
+}
+
+.segment-b, .segment-c {
+    align-self: flex-end;
+}
+
+.dot {
+   width: 12px;
+   height: 12px;
+   border-radius: 50%;
+   position: absolute;
+   bottom: 6px;
+   right: 4px;
+   background-color: #666;
+}
+
+.dot.active {
+   background-color: #f00;
 }
 </style>
